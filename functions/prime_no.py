c82ac5087d8295aa81059d9f799f8cb3de41f7d0
adhishwaran <aadhishwaran34@gmail.com>
2025-07-07 10:30:16 +0530

    hehe

--- functions/prime_no.py
+++ functions/prime_no.py
@@ -1,1 +1,14 @@
 import math
+
+n = int(input('enter an number: '))
+
+if n <= 1:
+    print(f'{n} is not a  prime')
+elif n>1 :
+    for i in range(2,n):
+        if n%i == 0:
+            print(f'{n} is not a prime number')
+            print(f'{n}  is the factor of {i}')
+            break
+    else:
+        print(f'{n} is prime')
