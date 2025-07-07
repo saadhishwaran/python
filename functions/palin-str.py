cfab7328eabdf90b93670560d2003aaff0b1e71a
adhishwaran <aadhishwaran34@gmail.com>
2025-07-07 11:34:09 +0530

    fff

--- functions/palin-str.py
+++ functions/palin-str.py
@@ -1,15 +1,15 @@
-def is_palindrome(s):
-    s = s.lower()  
-    length = len(s)
-    
-    for i in range(length // 2):
-        if s[i] != s[length - 1 - i]:
-            return False
-    return True
-
 a = input("Enter a word: ")
 
-if is_palindrome(a):
-    print(f'{a} is Palindrome')
+s = a.lower()  
+length = len(s)
+is_palin = True
+
+for i in range(length // 2):
+    if s[i] != s[length - 1 - i]:
+        is_palin = False
+        break
+
+if is_palin:
+    print(f'{a} is a palindrome.')
 else:
-    print(f'{a} is not a palindrome')
+    print(f'{a} is not a palindrome.')
