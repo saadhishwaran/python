c33803b5c961eb456783fa220c06c871f94d1afe
adhishwaran <aadhishwaran34@gmail.com>
2025-07-08 10:13:29 +0530

    repoitory

---
+++ functions/factorial.py
@@ -0,0 +1,11 @@
+a = int(input('enter a number: '))
+b = 1
+if a == 0 :
+    print('the value is: 1;')
+elif a == 1:
+    print('the value is: 1;')
+else:
+    for i in range(1,a+1):
+        b *= i
+    print('the value is:',b)
+
