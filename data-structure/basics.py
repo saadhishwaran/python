c5ba2b8d194dd90b7c4f00acf6d2bfe68404fa15
adhishwaran <aadhishwaran34@gmail.com>
2025-07-08 11:50:39 +0530

    po

---
+++ data-structure/basics.py
@@ -0,0 +1,12 @@
+list1 = []
+set1 = {5,6,7,8,9}
+set2 = {9,10,11,12,13}
+tuple1 = (1,2,3,3,4)
+list3 = list(tuple1)
+list1.extend(list3)
+set3 = set2.union(set1)
+list2 = list(set3)
+list1.extend(list2)
+print(list1)
+print(tuple1.count(3))
+print(tuple1.index(3))
---
+++ data-structure/employe/employe.py
@@ -0,0 +1,7 @@
+def functions(c,b):
+    a = c+b
+    print(a)
+
+b = 4
+c = 5
+functions(b,c) 
