44259583b7e751eae6a0de2c986c8c952cfed92b
adhishwaran <aadhishwaran34@gmail.com>
2025-07-10 11:27:41 +0530

    aad

---
+++ data-structure/employe/eemploye.py
@@ -0,0 +1,32 @@
+def create():
+    dict1 = {}
+    name = input('enter eemploye name: ')
+    age = int(input('enter eemploy age: '))
+    salary = float(input('enter eemploye salary: '))
+    postition = input('enter eemploye position: ')
+    dict1.update({'name':name,'age':age,'salary':salary,'position':postition})
+    list2.append(dict1)
+    print(dict1)
+    print(list2)
+
+def update():
+    dict1 = {}
+    name = input('enter eemploye name: ')
+    age = int(input('enter eemploy age: '))
+    salary = float(input('enter eemploye salary: '))
+    postition = input('enter eemploye position: ')
+    dict1.update({'name':name,'age':age,'salary':salary,'position':postition})
+    list2.append(dict1)
+    print(dict1)
+    print(list2)
+
+def search():
+    print('uhhhh')
+
+def display():
+    print('ohhhhh')
+
+List1 = [{'name':'aadthiya','age':30,'salary':100000.0,'position':'regional maneger'},{'name':'gokul','age':20,'salary':20000000.0,'position':'chief excecutive officer'}]
+list2 = []
+print(list2)
+print("____x____X____x____")
--- data-structure/employe/main.py
+++ data-structure/employe/main.py
@@ -1,16 +1,16 @@
 import employe
 
-print("to update the details enter:'update'")
-print("to push the value enter:'creat'")
+print("to update the details enter:'create'")
+print("to push the value enter:'update'")
 print("to search the particular element:'search'")
 print("to get all the values:'display'")
 
-op = input("enter any one of the given operation: ").lower()
+op = input("enter any one of the given operation: ").strip().lower()
 
-if op == 'update':
+if op == 'create':
+    employe.create()
+elif op == 'update':
     employe.update()
-elif op == 'creat':
-    employe.creat()
 elif op == 'search':
     employe.search()
 elif op == 'display':
