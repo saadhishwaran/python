19653b7c5fcf50dbfee75881117ae0f73d61e329
adhishwaran <aadhishwaran34@gmail.com>
2025-07-23 10:55:11 +0530

    changes

---
+++ for-projects/pass-gen.py
@@ -0,0 +1,14 @@
+import random
+import string
+
+def generator(length):
+    charr = name + ag 
+    for i in range(length) :
+        password = ''.join(random.choice(charr))
+        print(password,end = '')
+
+length = int(input('Enter how long the password should be: '))
+name = input('enter the name: ')
+age = input('enter year of birth: ')
+ag = str(age)
+generator(length)
