956e0624c9f428a6cb6c1c01d5cf0a11439ac110
adhishwaran <aadhishwaran34@gmail.com>
2025-07-28 11:46:17 +0530

    updata

---
+++ test/data2/teach_stor.py
@@ -0,0 +1,14 @@
+list1 = [
+    {
+        'name':'aadhi',
+        'age':19,
+        'course':'eee',
+        'exam': [
+            {
+                'sem': [{'sub':'eng', 'marks': 88},{'sub':'tam', 'marks': 89},{'sub':'phys','marks':87},{'sub':'maths','marks':86},{'sub':'computer','marks':85}],
+                'sem': [{'sub':'eng', 'marks': 88},{'sub':'tam', 'marks': 89},{'sub':'phys','marks':87},{'sub':'maths','marks':86},{'sub':'computer','marks':85}],
+                'sem': [{'sub':'eng', 'marks': 88},{'sub':'tam', 'marks': 89},{'sub':'phys','marks':87},{'sub':'maths','marks':86},{'sub':'computer','marks':85}]
+            }
+        ] 
+    }
+]
