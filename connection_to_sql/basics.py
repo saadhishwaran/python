dcab75e63981f31226ad396af094c081481c832d
sofia-dhanabal <aadhishwaran34@gmail.com>
2025-09-09 18:40:17 +0530

    vvv

--- connection_to_sql/basics.py
+++ connection_to_sql/basics.py
@@ -1,37 +1,13 @@
+
 import mysql.connector
-
-try:
-    mydb = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
-    )
-    if mydb.is_connected():
-        print("Connection established")
-except mysql.connector.Error as err:
-    print(f"Error: {err}")
-
-cursor = mydb.cursor()
-
-cursor.execute("CREATE DATABASE IF NOT EXISTS school_db")
-cursor.execute("USE school_db") 
-
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS students_data (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100),
-    age INT,
-    grade VARCHAR(10)
+conn = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password=""
 )
-""")
-cursor.execute("INSERT INTO students_data (name, age, grade) VALUES ('Alice', 14, '8th')")
-cursor.execute("INSERT INTO students_data (name, age, grade) VALUES ('Bob', 15, '9th')")
-
-mydb.commit()
-
-cursor.execute("SELECT * FROM students_data")
-for data in cursor.fetchall():
-    print(data)
-
-cursor.close()
-mydb.close()
+cursor = conn.cursor()
+cursor.execute("CREATE DATABASE IF NOT EXISTS Testdb")
+cursor.execute("USE Testdb")
+cursor.execute("CRESTE TABLE IF NOT EXISTS users(id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(30), email VARCHAR(30),)""")
+cursor.execute("INSERT INTO users(name, email) VALUES(%s, %s),('Aadhishwaran','Aashi@gmail.com'))")
+conn.commit()
