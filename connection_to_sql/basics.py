5fbc3740d8402a7e26158f35f02a0b1bead51c38
sofia-dhanabal <aadhishwaran34@gmail.com>
2025-09-09 18:57:32 +0530

    py

--- connection_to_sql/basics.py
+++ connection_to_sql/basics.py
@@ -3,11 +3,17 @@ import mysql.connector
 conn = mysql.connector.connect(
     host="localhost",
     user="root",
-    password=""
+    password="",
 )
 cursor = conn.cursor()
 cursor.execute("CREATE DATABASE IF NOT EXISTS Testdb")
 cursor.execute("USE Testdb")
-cursor.execute("CRESTE TABLE IF NOT EXISTS users(id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(30), email VARCHAR(30),)""")
-cursor.execute("INSERT INTO users(name, email) VALUES(%s, %s),('Aadhishwaran','Aashi@gmail.com'))")
+cursor.execute("""CREATE TABLE IF NOT EXISTS users(id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(30),email VARCHAR(30), )""")
+cursor.execute("INSERT INTO users(name, email) VALUES(%s, %s),('Aadhishwaran','Aashi@gmail.com')")
 conn.commit()
+cursor.execute("SELECT * FROM users")
+rows = cursor.fetchall()
+for row in rows:
+    print(row)
+
+conn.close()
